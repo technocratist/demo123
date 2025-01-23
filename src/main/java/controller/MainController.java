7e0a74e018a77bf4697edd987d2febede1c6b489
technocratist <technocratist666@gmail.com>
2025-01-23 17:41:03 +0900

    3rd

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,7 +1,17 @@
 package controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
+    public aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa getAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa() {
+
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                aaaaaaa
+
+    }
 }
